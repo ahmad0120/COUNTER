a0968c1edeca7feef4b49ff58fefa3c09f1cfff7
ahmad0120 <hmad121ahmad@gmail.com>
2025-06-17 02:40:42 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+function Counter() {
+  const [count, setCount] = useState(0);
+ return (
+   <div className="bg-green-300 flex justify-center items-center h-screen flex-col">
+  <h1 className="mb-4 text-2xl font-bold">Count: {count}</h1>
+  <div className="flex space-x-4">
+    <button
+      onClick={() => setCount(count + 1)}
+      className="px-4 py-2 bg-green-500 text-white rounded" >
+      Increase
+    </button>
+    <button
+      onClick={() => {
+        if (count > 0) {
+          setCount(count - 1);
+        }
+      }}
+      className={`px-4 py-2 rounded text-white ${count > 0 ? 'bg-red-500' : 'bg-gray-400 cursor-not-allowed'}`}
+      disabled={count === 0}
+    >
+      Decrease
+    </button>
+  </div>
+</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
   );
 }
 
-export default App;
+export default Counter;
